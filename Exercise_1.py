9994c25a0381237e2366ae4162da2ee56a4cfdce
LiamJones21 <92636294+LiamJones21@users.noreply.github.com>
2021-11-02 08:30:50 +0000

    Update Exercise_1.py

--- Exercise_1.py
+++ Exercise_1.py
@@ -2,3 +2,25 @@
 # This input needs to be an integer. Assign it to a variable named "n". The program will 
 # compute the product n*nn*nnn and print it on the screen.
 # example: n = 5 output = 5*55*555 = 152625
+
+def output(n):
+    index = 1
+    multiplication_Output = 1
+    while index < 4:
+        num = int(str(n)*index)
+        multiplication_Output = multiplication_Output * num
+        index = index + 1
+        
+
+    print (multiplication_Output)
+
+
+def enter_Value():
+    n = int(input("Please enter a integer"))
+    output(n)
+
+try:
+    enter_Value()
+except:
+    print('That was not a integer,')
+    enter_Value()
